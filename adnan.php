2358ae1346d6f4893dbfa2962b55405cf012c0ca
adnan <brac.adnan@gmail.com>
2017-05-10 16:35:29 +0200

    This file is for adding date to database

---
+++ adnan.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?>
+ <?php
+ echo "Today is" . date ("Y/m/d") . "<br>";
+ <?php
+ </body>
+</html>
+
